d4779c11714a6e630d05f2bfb051917221824c6f
Ankit singh <singhankit0460@gmail.com>
2024-06-30 02:17:17 +0530

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./scenes/logiinPage/Login";
 import Home from "./scenes/homePage/Home";
-
+import Profile from "./scenes/profilePage/Profile";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile/:userId" element={<Profile />} />
       </Routes>
     </>
   );
