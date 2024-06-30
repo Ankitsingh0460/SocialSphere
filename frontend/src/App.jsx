2172f136808a1f546634199532822a71f7521fbf
Ankit singh <singhankit0460@gmail.com>
2024-06-30 18:23:32 +0530

    redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./scenes/logiinPage/Login";
 import Home from "./scenes/homePage/Home";
 import Profile from "./scenes/profilePage/Profile";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import authReducer from "./state/App.js";
 import { configureStore } from "@reduxjs/toolkit";
