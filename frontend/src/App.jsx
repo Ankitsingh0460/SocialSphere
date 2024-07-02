5a2a44572f20eb2416418418b728b75c08da2b92
Ankit singh <singhankit0460@gmail.com>
2024-07-02 13:58:23 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,26 @@ import Login from "./scenes/logiinPage/Login";
 import Home from "./scenes/homePage/Home";
 import Profile from "./scenes/profilePage/Profile";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile/:userId" element={<Profile />} />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </ThemeProvider>
       </Routes>
     </>
   );
